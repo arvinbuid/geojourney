34ced421e651dc87669d191a92ac416da6f920f5
Arvin Buid <arvinbuid01@gmail.com>
2024-07-31 19:09:28 +0800

    Created AppLayout, AppNav components, AppNav and PageNav css module and import them to its respective jsc component where it belongs to.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
+import {Link} from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
+      <AppNav />
       <PageNav />
-      Homepage
+      <h1>Geo Journey</h1>
+      <Link to='/app'>Go to the app</Link>
     </div>
   );
 }
