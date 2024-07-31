cb8ec6b42984dafd66c17d222ae1ac22cc1ff0eb
Arvin Buid <arvinbuid01@gmail.com>
2024-07-31 17:23:21 +0800

    Created a NotFound component and routes for Homepage, Product, Pricing, and NotFound.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import NotFound from "./pages/NotFound";
+
 export default function App() {
-  return <div>Geo Journey</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
