c3d88ea82f2047e598f02844b2c6655ef46491f9
Arvin Buid <arvinbuid01@gmail.com>
2024-08-01 18:47:18 +0800

    Created Logo and Login component and its css module for styling. Updated Pricing, Product, Homepage components, bg and logo images.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,18 +1,24 @@
+import {Link} from "react-router-dom";
 import styles from "./Homepage.module.css";
+import PageNav from "../components/PageNav";
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
+      <PageNav />
       <section>
         <h1>
           You travel the world.
           <br />
-          GeoJourney keeps track of your adventures.
+          GeoJourney keeps track of all your adventures.
         </h1>
         <h2>
-          A world map that tracks your footsteps into every city you can think of. Never forget your
-          wonderful experiences, and show your friends how you have wandered the world.
+          A world map that tracks your footsteps through every city you can think of. Never forget
+          your amazing adventures, and show friends how you&apos;ve traveled the world.
         </h2>
+        <Link to='/app' className='cta'>
+          Start tracking now
+        </Link>
       </section>
     </main>
   );
