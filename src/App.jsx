b41f6c48b398cad521f573d9b0d29976cc485cc9
Arvin Buid <arvinbuid01@gmail.com>
2024-08-02 15:43:33 +0800

    Installed json server third party package to act as a server of the fake cities data. Created CityList component and use it inside AppLayout children component.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Pricing from "./pages/Pricing";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import CityList from "./components/CityList";
 
 export default function App() {
   return (
@@ -16,8 +17,8 @@ export default function App() {
           <Route path='pricing' element={<Pricing />} />
           <Route path='login' element={<Login />} />
           <Route path='app' element={<AppLayout />}>
-            <Route index element={<p>List of cities</p>} />
-            <Route path='cities' element={<p>List of cities</p>} />
+            <Route index element={<CityList />} />
+            <Route path='cities' element={<CityList />} />
             <Route path='countries' element={<p>Countries</p>} />
             <Route path='form' element={<p>Form</p>} />
           </Route>
