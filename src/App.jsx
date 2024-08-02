2ac119a8e928817668a7f724c457ac1c178b89cc
Arvin Buid <arvinbuid01@gmail.com>
2024-08-02 10:40:57 +0800

    Created nested routes of app path inside the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,11 @@ export default function App() {
           <Route path='product' element={<Product />} />
           <Route path='pricing' element={<Pricing />} />
           <Route path='login' element={<Login />} />
-          <Route path='app' element={<AppLayout />} />
+          <Route path='app' element={<AppLayout />}>
+            <Route path='cities' element={<p>Cities</p>} />
+            <Route path='countries' element={<p>Countries</p>} />
+            <Route path='form' element={<p>Form</p>} />
+          </Route>
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
