3964b1190b571782fd130073d773d34145946c56
Arvin Buid <arvinbuid01@gmail.com>
2024-08-02 11:02:45 +0800

    Added index route inside the app route path in App component. Utilized the Outlet component built-in from react router to display all of the children routes to the Sidebar.
    Created a list of tabs inside AppNav component named Cities and Countries.

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,13 @@ export default function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Homepage />} />
+          <Route index element={<Homepage />} />
           <Route path='product' element={<Product />} />
           <Route path='pricing' element={<Pricing />} />
           <Route path='login' element={<Login />} />
           <Route path='app' element={<AppLayout />}>
-            <Route path='cities' element={<p>Cities</p>} />
+            <Route index element={<p>List of cities</p>} />
+            <Route path='cities' element={<p>List of cities</p>} />
             <Route path='countries' element={<p>Countries</p>} />
             <Route path='form' element={<p>Form</p>} />
           </Route>
