5943264b293722773f656a729317febbaa84ace6
Arvin Buid <arvinbuid01@gmail.com>
2024-08-03 15:37:52 +0800

    Reading and settting a query string using useSearchParams() hook inside of Map and City component to display the latitude and longitude of a city.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,21 @@
+import {useSearchParams} from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  let lat = searchParams.get("lat");
+  let lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h2>
+        Position: lat: {lat}, long: {lng}
+      </h2>
+      <button onClick={() => setSearchParams({lat: 22, lng: 40})}>Change Position</button>
+    </div>
+  );
 }
 
 export default Map;
