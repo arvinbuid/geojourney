a93ef40a58234273fdd098ffc8b26269809a162c
Arvin Buid <arvinbuid01@gmail.com>
2024-08-04 21:11:54 +0800

    Added map placeholder to display a map in the Map component.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,11 @@
 import {useNavigate, useSearchParams} from "react-router-dom";
 import styles from "./Map.module.css";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import {useState} from "react";
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
 
   let lat = searchParams.get("lat");
   let lng = searchParams.get("lng");
@@ -11,11 +14,17 @@ function Map() {
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h2>
-        Position: lat: {lat}, long: {lng}
-      </h2>
-      <button onClick={() => setSearchParams({lat: 22, lng: 40})}>Change Position</button>
+      <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
