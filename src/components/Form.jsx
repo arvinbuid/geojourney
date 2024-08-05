94e0da8662429fbdec5c9bf99d90d62fda617a89
Arvin Buid <arvinbuid01@gmail.com>
2024-08-05 16:42:08 +0800

    Added a guard clause inside the Form component and useEffect.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -30,6 +30,8 @@ function Form() {
   const [geocodingError, setGeocodingError] = useState("");
 
   useEffect(() => {
+    if (!lat && !lng) return;
+
     async function fetchCityData() {
       try {
         setIsLoadingGeoCoding(true);
@@ -54,6 +56,8 @@ function Form() {
   }, [lat, lng]);
 
   if (isLoadingGeocoding) return <Spinner />;
+
+  if (!lat && !lng) return <Message message='Start by clicking somewhere on the map.' />;
 
   if (geocodingError) return <Message message={geocodingError} />;
 
