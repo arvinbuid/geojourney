4e96fc0cd5571faa1fecb55c0700340c8c730122
Arvin Buid <arvinbuid01@gmail.com>
2024-08-06 16:02:19 +0800

    Changed the url to uploaded json server on render on CitiesContext.jsx

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,6 +1,6 @@
 import {createContext, useContext, useEffect, useReducer} from "react";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://geojourney-api.onrender.com";
 
 const CitiesContext = createContext();
 
